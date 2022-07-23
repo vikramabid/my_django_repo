6742612500d52dc73b77932067e065cf133de46a
Vikram Abid <vikramabid012@gmail.com>
2022-07-23 18:09:41 +0530

    fc1

---
+++ ormproject1/populate.py
@@ -0,0 +1,20 @@
+from faker import Faker
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','ormproject1.settings')
+import django
+django.setup()
+
+from testapp.models import Employee
+
+from random import *
+faker=Faker()
+def populate(n):
+  for i in range(n):
+     feno=randint(1001,9999)
+     fename=faker.name()
+     fesal=randint(10000,20000)
+     feaddr=faker.city()
+     emp_record = Employee.objects.get_or_create(eno=feno,ename=fename,esal=fesal,eaddr=feaddr)
+n= int(input("Enter Number of Records: "))
+populate(n)
+print(f"{n} Records Inserted successfully ")
---
+++ ormproject1/testapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.db.models.functions import Lower
+from testapp import views
+from testapp.models import Employee
+from django.db.models import Q
+# Create your views here.
+def retreve_view(request):
+  #emp_list=Employee.objects.all()
+
+  emp_list = Employee.objects.all().order_by('ename')
+
+
+  #emp_list = Employee.objects.filter(esal__gt=15000)
+  #emp_list = Employee.objects.filter(esal__gte=15000)
+  #emp_list = Employee.objects.filter(ename__contains="John")
+  #emp_list = Employee.objects.filter(id__in=[1,3,5])
+  #emp_list = Employee.objects.all.only('ename','esal')
+  print(type(emp_list))
+  return render(request,'testapp/index.html',{'emp_list':emp_list})
+from django.db.models import Avg,Sum,Max,Min,Count
+def display_view(request):
+  avg=Employee.objects.all().aggregate(Avg('esal'))
+  max = Employee.objects.all().aggregate(Max('esal'))
+  min = Employee.objects.all().aggregate(Min('esal'))
+  sum = Employee.objects.all().aggregate(Sum('esal'))
+  count = Employee.objects.all().aggregate(Count('esal'))
+  my_dict={'avg':avg,'max':max['esal__max'],'min':min['esal__min'],'sum':sum['esal__sum'],'count':count['esal__count']}
+  return render(request,'testapp/aggregate.html',my_dict)
